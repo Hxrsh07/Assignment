a66c71d97a028e9452f1b00070ac6adb24a5b94f
Harshit Gautam <82574368+Hxrsh07@users.noreply.github.com>
2023-05-26 15:28:01 +0530

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,76 @@
+const mobileNav = document.querySelector(".nav-container-mobile");
+const burger = document.querySelector(".nav-toggle");
+const burgerImg = burger.querySelector("img");
+const artList = document.querySelector(".art-list");
+const artTemplate = document.getElementById("art-info");
+const loading = document.querySelector(".loading-container");
+const main = document.querySelector(".main");
+
+const loadData = () => {
+  let artData = {};
+  fetch("https://api.artic.edu/api/v1/artworks?page=1&limit=9")
+    .then((response) => response.json())
+    .then((data) => {
+      loading.classList.add("hidden");
+      artData = data.data;
+
+      
+      if (Object.keys(artData).length > 0) {
+        main.classList.remove("hidden");
+      }
+      artData.forEach((element) => {
+        const artBody = document.importNode(artTemplate.content, true);
+        artBody.querySelector(
+          "img"
+        ).src = `https://www.artic.edu/iiif/2/${element.image_id}/full/843,/0/default.jpg`;
+        artBody.querySelector(".artist").textContent = element.artist_title;
+        artBody.querySelector(".img-desc").textContent = element.title;
+        artBody.querySelector(".likes").textContent = Math.floor(
+          Math.random() * 100
+        ).toString();
+        artBody.querySelector(".views").textContent =
+          (Math.floor(Math.random() * (1000 - 100) + 100) / 100).toString() +
+          "k";
+        artList.append(artBody);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const mobileNavHandler = (e) => {
+  const navItem = e.target.tagName === "H3" ? e.target : null;
+  if (!navItem) {
+    return;
+  }
+  navItem.nextElementSibling.classList.toggle("hidden");
+  if (navItem.getAttribute("data-showList") === "0") {
+    navItem.style.setProperty(
+      "--arrow-image-up",
+      "url('../images/arrow-up.svg')"
+    );
+    navItem.setAttribute("data-showList", "1");
+  } else {
+    navItem.style.setProperty(
+      "--arrow-image-up",
+      "url('../images/arrow-down.svg')"
+    );
+    navItem.setAttribute("data-showList", "0");
+  }
+};
+
+const burgerMenuHandler = () => {
+  mobileNav.classList.toggle("hidden");
+  if (burgerImg.getAttribute("data-showList") === "0") {
+    burgerImg.setAttribute("data-showList", "1");
+    burgerImg.setAttribute("src", "./images/close.svg");
+  } else {
+    burgerImg.setAttribute("data-showList", "0");
+    burgerImg.setAttribute("src", "./images/burger.svg");
+  }
+};
+
+loadData();
+mobileNav.addEventListener("click", mobileNavHandler);
+burger.addEventListener("click", burgerMenuHandler);
